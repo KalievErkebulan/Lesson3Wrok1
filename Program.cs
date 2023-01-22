41a9cb32eee9402f2739984545a5ba7a8ab06d91
Erkebulan <kaliev.e.m@gmail.com>
2023-01-23 05:24:27 +0600

    Lesson3Work2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintPossibleCoordinateOfPoint(int possibleCoordinateOfPoint)
+{
+    if(possibleCoordinateOfPoint == 1)
+        Console.WriteLine("x > 0 and y > 0");
+    if(possibleCoordinateOfPoint == 2)
+        Console.WriteLine("x < 0 and y > 0");
+    if(possibleCoordinateOfPoint == 3)
+        Console.WriteLine("x < 0 and y < 0");
+    if(possibleCoordinateOfPoint == 4)
+        Console.WriteLine("x > 0 and y < 0");
+}
+int possibleCoordinateOfPoint = int.Parse(Console.ReadLine()!);
+PrintPossibleCoordinateOfPoint(possibleCoordinateOfPoint);
