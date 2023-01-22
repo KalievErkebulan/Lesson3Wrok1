0d0f59dfc28ba5881cc44fecd1f99dc5f2bb6630
Erkebulan <kaliev.e.m@gmail.com>
2023-01-23 01:40:44 +0600

    Lesson3SeminarWork1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int getNumberOfPlane(double x, double y)
+{
+    int numberOfPlane = -1;
+
+    if (x > 0 && y > 0)
+    numberOfPlane = 1;
+    
+    if (x < 0 && y > 0)
+    numberOfPlane = 2;
+
+    if (x < 0 && y < 0)
+    numberOfPlane = 3;
+
+    if (x > 0 && y < 0)
+    numberOfPlane = 4;
+    return numberOfPlane;
+}
+Console.WriteLine("X:");
+double x = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Y:");
+double y = double.Parse(Console.ReadLine()!);
+
+int numberOfPlane = getNumberOfPlane(x, y);
+
+if (numberOfPlane == -1)
+    Console.WriteLine("Вы ввели некорректные координаты");
+else
+    Console.WriteLine($"Номер плоскости: {numberOfPlane}.");
+
